a3badc59fade684b623a391ba6e7f0d4275232fc
kratos <oclockvn@gmail.com>
2015-04-14 22:16:46 +0700

    upload

---
+++ MultipleUploadFileExample/Controllers/UploadController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultipleUploadFileExample.Controllers
+{
+    public class FileUpload
+    {
+        public FileUpload()
+        {
+            this.FileUploads = new List<HttpPostedFileBase>();
+        }
+
+        public List<HttpPostedFileBase> FileUploads { get; set; }
+    }
+
+    public class UploadController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(FileUpload model)
+        {
+            foreach (var file in model.FileUploads)
+            {
+                if (file != null && file.ContentLength > 0)
+                {
+                    var path = Server.MapPath("~/Content/" + file.FileName);
+                    file.SaveAs(path);
+                }
+            }
+
+            return View("Index");
+        }
+    }
+}
